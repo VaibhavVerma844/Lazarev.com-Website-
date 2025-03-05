c6eaaeabc8bbfb1276b81c2b5e23ad93d6236a70
Vaibhav Verma <vermavaibhav452@gmail.com>
2025-03-05 12:24:24 +0530

    message

---
+++ main.js
@@ -0,0 +1,40 @@
+function navAnimation(){
+    var nav = document.querySelector("nav")
+
+nav.addEventListener("mouseenter",function(){
+    let tl = gsap.timeline()
+
+    tl.to(".nav_bottom",{
+        height:"21vh"
+    })
+    tl.to("nav .navlinks h5",{
+        display:"block"
+    })
+    tl.to(".navlinks h5 span",{
+        y:0,
+        stagger:{
+            amount:0.5
+        }
+    })
+})
+
+nav.addEventListener("mouseleave",function(){
+    let tl = gsap.timeline()
+    tl.to(".navlinks h5 span",{
+        y:25,
+        stagger:{
+            amount:0.2
+        }
+    })
+    tl.to("nav .navlinks h5",{
+        display:"none",
+        duration:0.1
+    })
+    tl.to(".nav_bottom",{
+        height:0,
+        duration:0.3
+    })
+})
+}
+navAnimation()
+
